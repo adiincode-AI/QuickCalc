0b7b54a9eb0c1892fff03d7a8b2ccb087b2d0dd4
Aditya <arout2922@gmail.com>
2026-04-26 19:13:40 +0530

    Final Code

--- calculator.py
+++ calculator.py
@@ -1,35 +1,33 @@
-def add(a, b):
-    print(f"{a} + {b}= ", a + b)
+import streamlit as st
 
+st.header("Calculator")
+col1, col2 = st.columns(2)
+with col1:
+    num1 = st.number_input("Enter first number", value=0.00)
+with col2:
+    num2 = st.number_input("Enter second number", value=0.00)
 
-def sub(a, b):
-    print(a - b)
+col1, col2, col3, col4 = st.columns(4)
 
+result = None
 
-def mul(a, b):
-    print(a * b)
+with col1:
+    if st.button("+"):
+        result = num1 + num2
+with col2:
+    if st.button("-"):
+        result = num1 - num2
+with col3:
+    if st.button("x"):
+        result = num1 * num2
+with col4:
+    if st.button("÷"):
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error"
+        
 
+if result is not None:
+    st.success(f"Result:{result}")
 
-def div(a, b):
-    print(a / b)
-
-
-while True:
-    user_input1 = int(input("Enter Number:"))
-    user_input2 = int(input("Enter Number: "))
-    print(f"1st Number= {user_input1} 2nd Number = {user_input2}")
-
-    print("\n1-Addition\n 2-Subtraction\n 3-Multiplication\n 4-Division")
-
-    operation = int(input("Choose Operation: "))
-
-    if operation == 1:
-        add(user_input1, user_input2)
-    elif operation == 2:
-        sub(user_input1, user_input2)
-    elif operation == 3:
-        mul(user_input1, user_input2)
-    elif operation == 4:
-        div(user_input1, user_input2)
-    else:
-        print("Enter a valid number")
